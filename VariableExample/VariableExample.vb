876bd4f1bc916943d0cb5dd05aa23cd8df2697b3
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-01 08:44:07 -0600

    Add header info

---
+++ VariableExample/VariableExample.vb
@@ -0,0 +1,18 @@
+﻿'Elliot Heiner
+'Fall 2021
+'Variable and Data Type Example
+'https://github.com/heinelli/VariableExample.git
+
+
+
+Module VariableExample
+
+    Sub Main()
+
+
+
+
+
+    End Sub
+
+End Module
